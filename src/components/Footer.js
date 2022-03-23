b7a4283f6dd1300aeeab8d5eca7254c90e66f710
Raffi Pratama <73082791+Raffi1412@users.noreply.github.com>
2022-03-23 20:03:20 +0700

    Add files via upload
    Tugas Pertemuan 4

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+        <footer>
+            <h2>Copyright @raff_1412</h2>
+            <p>Created By React.js</p>
+        </footer>
+    );
+}
+
+export default Footer;
